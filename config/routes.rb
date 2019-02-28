47025d831a208afd0e542d78a44eb2041996f964
Anthony Klor <antklor@gmail.com>
2019-02-28 17:19:26 -0300

    dfdaaa

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'cocktails#index'
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+    resources :reviews, only: [:index, :new, :create]
+  end
+
+  resources :doses, only: [:destroy]
+  resources :reviews, only: [:show, :edit, :update, :destroy]
 end
